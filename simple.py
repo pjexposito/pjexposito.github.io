e74ee9c43628349a4ad3bedc253b3203aa4880d9
pjexposito <pjexposito@gmail.com>
2020-03-10 10:33:36 +0100

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,17 @@
+import simplenote
+import json
+import base64
+    
+        
+    
+print ("Cargando datos...")
+sn = simplenote.Simplenote("pjexposito@gmail.com","E13n1+4s")
+todo=sn.get_note_list(data=True, tags=[])
+en_json = json.dumps(todo, indent=4)
+#print (en_json)
+mensaje = en_json.encode("utf-8")
+encoded = base64.b64encode(base64.b64encode(mensaje))
+file1 = open("/data/data/com.termux/files/home/pjexposito.github.io/datos.json","w") 
+file1.write(encoded.decode("utf-8")) 
+file1.close() 
+print ("Archivo creado. Subiendo a Github...")
