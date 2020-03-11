19afaf793129538aaf57e5f0b80f813e9bd76ff1
Pedro <pjexposito@gmail.com>
2020-03-11 12:50:07 +0100

    Actualizado

--- simple.py
+++ simple.py
@@ -1,6 +1,8 @@
 import simplenote
 import json
 import base64
+from itertools import cycle
+key = 'simplenote'
     
         
     
@@ -9,9 +11,14 @@ sn = simplenote.Simplenote("pjexposito@gmail.com","E13n1+4s")
 todo=sn.get_note_list(data=True, tags=[])
 en_json = json.dumps(todo, indent=4)
 #print (en_json)
-mensaje = en_json.encode("utf-8")
-encoded = base64.b64encode(base64.b64encode(mensaje))
+#mensaje = en_json.encode("utf-8")
+#encoded = base64.b64encode(base64.b64encode(mensaje))
+print (en_json)
+mensaje = ''.join(chr(ord(c)^ord(k)) for c,k in zip(en_json, cycle(key)))
+mensaje = base64.b64encode(mensaje.encode("utf-8"))
+print('%s' % (mensaje))
+
 file1 = open("/data/data/com.termux/files/home/pjexposito.github.io/datos.json","w") 
-file1.write(encoded.decode("utf-8")) 
+file1.write(mensaje) 
 file1.close() 
 print ("Archivo creado. Subiendo a Github...")
