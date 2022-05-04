6f232f4d9a86ec7f66866babe38acd639938c500
pjexposito <pjexposito@gmail.com>
2022-05-04 12:55:15 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,7 @@ Copyright 2015, 2019 Google Inc. All Rights Reserved.
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = 'offline';
 // Customize this with a different URL if needed.
-const OFFLINE_URL = 'fpp.html';
+const OFFLINE_URL = 'fpp-mini.html';
 
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
